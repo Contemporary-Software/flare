164c31cb0e414fc554c026306a63cc9b7ca36468
Chris Johns <chrisj@rtems.org>
2025-10-29 14:09:53 +1100

    build: Use a tools prefix and not path and the exe-prefix has the last -
    These align the configure options with how they are normally used.

--- buildcontrol.py
+++ buildcontrol.py
@@ -2,6 +2,7 @@
 # Flare Build Control
 #
 
+import os
 
 def recurse(ctx, directories):
     for d in directories:
@@ -14,7 +15,7 @@ def options(opt):
     copts.add_option('--tools-path',
                      default=None,
                      dest='flare_tools_path',
-                     help='Path to tools')
+                     help='Path to tools (installed prefix)')
     copts.add_option('--compiler-prefix',
                      default=None,
                      dest='flare_compiler_prefix',
@@ -48,25 +49,28 @@ def configure(conf):
     if conf.options.flare_tools_path == None:
         tool_path_list = os.environ['PATH']
     else:
-        tool_path_list = conf.options.flare_tools_path
+        if os.path.exists(os.path.join(conf.options.flare_tools_path, 'bin')):
+            tool_path_list = os.path.join(conf.options.flare_tools_path, 'bin')
+        else:
+            tool_path_list = conf.options.flare_tools_path
 
-    conf.find_program(tools_prefix + '-gcc',
+    conf.find_program(tools_prefix + 'gcc',
                       path_list=tool_path_list,
                       var="CC")
-    conf.find_program(tools_prefix + '-g++',
+    conf.find_program(tools_prefix + 'g++',
                       path_list=tool_path_list,
                       var="CXX")
-    conf.find_program(tools_prefix + '-gcc',
+    conf.find_program(tools_prefix + 'gcc',
                       path_list=tool_path_list,
                       var="LINK_CC")
-    conf.find_program(tools_prefix + '-g++',
+    conf.find_program(tools_prefix + 'g++',
                       path_list=tool_path_list,
                       var="LINK_CXX")
-    conf.find_program(tools_prefix + '-gcc',
+    conf.find_program(tools_prefix + 'gcc',
                       path_list=tool_path_list,
                       var="AS")
-    conf.find_program(tools_prefix + '-ld', path_list=tool_path_list, var="LD")
-    conf.find_program(tools_prefix + '-ar', path_list=tool_path_list, var="AR")
+    conf.find_program(tools_prefix + 'ld', path_list=tool_path_list, var="LD")
+    conf.find_program(tools_prefix + 'ar', path_list=tool_path_list, var="AR")
 
     conf.load('gcc')
     conf.load('g++')
